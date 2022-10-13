d3c9fa79a6c36f8be8ffcafdcbacaaef37cd8113
Oleksandr Pecheniuk <oleksandr@ai-omatic.com>
2022-10-13 16:58:36 +0200

    add: pagination for StartUpList

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import StartupCard from "./StartupCard";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { useGetData } from "../../hooks/useGetData.hook";
 import { MainContext } from "../../context/index";
+import { Pagination } from "@mui/material";
 
 const StartupList: React.FC = () => {
   const { getStartUpList } = useGetData();
+
+  const [page, setPage] = useState(0);
   const { startUpList } = useContext(MainContext);
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value - 1);
+  };
   useEffect(() => {
     getStartUpList();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -14,9 +20,10 @@ const StartupList: React.FC = () => {
 
   return (
     <>
-      {startUpList.map((startUpItem) => (
+      {startUpList?.slice(page * 20, page * 20 + 20).map((startUpItem) => (
         <StartupCard data={startUpItem} key={startUpItem.id} />
       ))}
+      <Pagination count={10} variant="outlined" onChange={handleChange} />
     </>
   );
 };
